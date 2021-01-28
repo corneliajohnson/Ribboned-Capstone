29ce31d89341312e756654963e49264dd4950471
corneliajohnson <corneliaj91@gmail.com>
2021-01-28 13:10:49 -0500

    added video to page

--- Ribboned/clientside/src/components/Ribbon/RibbonCard.js
+++ Ribboned/clientside/src/components/Ribbon/RibbonCard.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Moment from "react-moment";
+import ReactPlayer from "react-player";
 
 import {
   Card,
-  CardImg,
   CardText,
   CardBody,
   CardTitle,
@@ -11,21 +12,46 @@ import {
 } from "reactstrap";
 
 export const RibbonCard = ({ ribbon }) => (
-  <div className="col-3">
-    <Card>
-      <CardImg
+  <div className="col-4">
+    <Card style={{ height: "500px" }}>
+      <ReactPlayer
         top
         width="100%"
+        height="60%"
         src="/assets/318x180.svg"
         alt="Card image cap"
+        controls={false}
+        url={ribbon.url}
       />
       <CardBody>
         <CardTitle tag="h5">{ribbon.title}</CardTitle>
         <CardSubtitle tag="h6" className="mb-2 text-muted">
-          {ribbon.dateCreated}
+          <Moment fromNow>{ribbon.dateCreated}</Moment>
         </CardSubtitle>
         <CardText>{ribbon.decription}</CardText>
-        <Button>Button</Button>
+        {ribbon.snags.length === 0 ? (
+          "No Ribbon Snags"
+        ) : (
+          <>
+            <CardText>Ribbon Snags</CardText>
+            {ribbon.snags[0] ? (
+              <CardText>
+                {ribbon.snags[0].note}{" "}
+                <Moment fromNow>{ribbon.snags[0].dateCreated}</Moment>{" "}
+              </CardText>
+            ) : (
+              ""
+            )}
+            {ribbon.snags[1] ? (
+              <CardText>
+                {ribbon.snags[1].note}{" "}
+                <Moment fromNow>{ribbon.snags[1].dateCreated}</Moment>{" "}
+              </CardText>
+            ) : (
+              ""
+            )}
+          </>
+        )}
       </CardBody>
     </Card>
   </div>
