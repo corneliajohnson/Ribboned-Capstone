fd27080c8eb546151d6f092660843afdcdce02c2
corneliajohnson <corneliaj91@gmail.com>
2021-01-29 15:30:22 -0500

    added form styling

--- Ribboned/clientside/src/components/Ribbon/RibbonForm.js
+++ Ribboned/clientside/src/components/Ribbon/RibbonForm.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import "./Ribbon.css";
 
 export const RibbonForm = (props) => {
+  const [isPublic, setIsPublic] = useState(false);
+  const handlePrivacy = () => {
+    if (!isPublic) {
+      setIsPublic(true);
+    } else {
+      setIsPublic(false);
+    }
+    console.log(isPublic);
+  };
   return (
     <div className="container">
       <div className="w-75 mx-auto p-4">
         <Form className="border p-5">
           <h2 className="text-center">New Ribbon</h2>
           <FormGroup row>
-            <Label for="exampleEmail" sm={2}>
+            <Label for="exampleEmail" lg={2}>
               Title
             </Label>
-            <Col sm={10}>
+            <Col lg={10}>
               <Input
                 type="email"
                 name="email"
@@ -24,10 +33,10 @@ export const RibbonForm = (props) => {
           <Row form>
             <Col md={6}>
               <FormGroup row>
-                <Label for="exampleEmail" sm={2}>
+                <Label for="exampleEmail" lg={2}>
                   Category
                 </Label>
-                <Col sm={10}>
+                <Col lg={10}>
                   <Input
                     type="email"
                     name="email"
@@ -39,10 +48,10 @@ export const RibbonForm = (props) => {
             </Col>
             <Col md={6}>
               <FormGroup row>
-                <Label for="exampleEmail" sm={2}>
+                <Label for="exampleEmail" lg={2}>
                   Source
                 </Label>
-                <Col sm={10}>
+                <Col lg={10}>
                   <Input
                     type="email"
                     name="email"
@@ -54,10 +63,10 @@ export const RibbonForm = (props) => {
             </Col>
           </Row>
           <FormGroup row>
-            <Label for="exampleEmail" sm={2}>
+            <Label for="exampleEmail" lg={2}>
               URL
             </Label>
-            <Col sm={10}>
+            <Col gl={10}>
               <Input
                 type="email"
                 name="email"
@@ -67,10 +76,10 @@ export const RibbonForm = (props) => {
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label for="exampleEmail" sm={2}>
+            <Label for="exampleEmail" lg={2}>
               Decription
             </Label>
-            <Col sm={10}>
+            <Col lg={10}>
               <Input
                 type="email"
                 name="email"
@@ -80,10 +89,14 @@ export const RibbonForm = (props) => {
             </Col>
           </FormGroup>
           <FormGroup>
-            <div class="button b2" id="button-18">
-              <input type="checkbox" class="checkbox" />
-              <div class="knobs"></div>
-              <div class="layer"></div>
+            <div className="button b2 m-1" id="button-18">
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={handlePrivacy}
+              />
+              <div className="knobs"></div>
+              <div className="layer"></div>
             </div>
           </FormGroup>
         </Form>
