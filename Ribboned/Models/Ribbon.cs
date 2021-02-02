667180b2e65f89e483965da367c78770bad45e73
corneliajohnson <corneliaj91@gmail.com>
2021-02-02 10:19:10 -0500

    added annotations

--- Ribboned/Models/Ribbon.cs
+++ Ribboned/Models/Ribbon.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ribboned.Models
 {
     public class Ribbon
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Decription { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public int SourceId { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string URL { get; set; }
+        [MaxLength(255)]
         public string Thumbnail { get; set; }
+        [Required]
         public bool IsActive { get; set; }
+        [Required]
         public bool IsPublic { get; set; }
+        [Required]
         public DateTime DateCreated { get; set; }
         public List<Snag> Snags { get; set; }
         public Category Category { get; set; }
--- Ribboned/Models/Snag.cs
+++ Ribboned/Models/Snag.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ribboned.Models
 {
     public class Snag
     {
         public int Id { get; set; }
+        [Required]
         public int RibbonId { get; set; }
+        [Required]
+        [MaxLength(500)]
         public string Note { get; set; }
+        [Required]
         public DateTime DateCreated { get; set; }
+        [Required]
         public int Seconds { get; set; }
+        [Required]
         public string TimeString { get; set; }
         public Ribbon Ribbon { get; set; }
     }
